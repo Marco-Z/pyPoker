1e5aa660b942b97d4583d9384bee76a732c2b6e6
Marco-Z <marcozugliani94@gmail.com>
2020-07-04 12:38:18 +0200

    Increase blinds on rotation

--- game.py
+++ game.py
@@ -1,20 +1,31 @@
 from player import Player
 from round import Round
+from math import floor
 
 
 class Game:
     def __init__(self, players, initial_small_blind=10):
         self.players = players
         self.small_blind = 10
+        self.rounds_played = 0
 
     def get_players_with_money(self):
         return [p for p in self.players if p.money > 0]
 
+    def get_small_blind(self):
+        return self.small_blind * 2 ** floor(self.rounds_played / len(self.players))
+
+    def finish_round(self):
+        input()
+        self.rounds_played += 1
+
     def play(self):
         while len(self.get_players_with_money()) > 1:
-            poker_round = Round(players=self.players, small_blind=self.small_blind)
+            poker_round = Round(
+                players=self.players, small_blind=self.get_small_blind()
+            )
             poker_round.play()
-            input()
+            self.finish_round()
 
 
 if __name__ == "__main__":
